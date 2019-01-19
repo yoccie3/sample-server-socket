4201d97f57c3a3c1288bc9e48d24fbc1aeaaf5a6
yoccie <yoccie.300.yards@gmail.com>
2019-01-19 21:48:01 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import socket
+from contextlib import closing
+
+
+def main():
+    host = '127.0.0.1'
+    port = 4000
+    backlog = 1
+    bufsize = 4096
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with closing(sock):
+        sock.bind((host, port))
+        sock.listen(backlog)
+        while True:
+            conn, address = sock.accept()
+            print(address)
+            with closing(conn):
+                msg = conn.recv(bufsize)
+                print(msg)
+                conn.send(msg)
+    return
+
+if __name__ == '__main__':
+  main()
